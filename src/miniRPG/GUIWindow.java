57744235981936ab902520d9b90c5fa1c22c158b
EVAN\Evan Snitkey <edude15000@hotmail.com>
2017-08-10 23:07:11 -0500

    Bug fixes, added load checking to gui, added mode select to gui, refactored level display on gui

--- src/miniRPG/GUIWindow.java
+++ src/miniRPG/GUIWindow.java
@@ -1,7 +1,16 @@
 package miniRPG;
 
-import java.awt.*;  // Using Frame class in package java.awt
-import java.awt.event.*;
+// Using Frame class in package java.awt
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.IOException;
 
 /* 
@@ -10,28 +19,27 @@ import java.io.IOException;
 
 // A GUI program is written as a subclass of Frame - the top-level container
 // This subclass inherits all properties from Frame, e.g., title, icon, buttons, content-pane
-public class GUIWindow extends Frame
-	implements ActionListener, WindowListener {
-    
-	//Create player
+public class GUIWindow extends Frame implements ActionListener, WindowListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// Create player
 	protected User user;
-	
+
 	// Declare Label components for home menu
-	private Label fishingLabel;
-	private Label fishingXpLabel; 
-	private Label cookingLabel;
-	private Label cookingXpLabel;
-	private Label miningLabel;
-	private Label miningXpLabel;
-	private Label smithingLabel;
-	private Label smithingXpLabel;
-	private Label herbloreXpLabel;
-	private Label herbloreLabel;
+	private Label fishingLabel = new Label();
+	private Label cookingLabel = new Label();
+	private Label miningLabel = new Label();
+	private Label smithingLabel = new Label();
+	private Label herbloreLabel = new Label();
 
 	// Declare console output
 	private TextField messages;
-	private String status; //for string to post to messages
-	
+	private String status; // for string to post to messages
+
 	// Declare button options - home menu
 	private Button combatSelectButton;
 	private Button InventorySelectButton;
@@ -42,168 +50,187 @@ public class GUIWindow extends Frame
 	private Button MiningSelectButton;
 	private Button QuitSelectButton;
 	private Button combatStatsSelectButton;
-	
+
 	public void updateLevels() {
 		fishingLabel.setText(Integer.toString(user.getLevel("Fishing")));
-		fishingXpLabel.setText(Integer.toString(user.getLevelObject("Fishing").getXp()));
 		cookingLabel.setText(Integer.toString(user.getLevel("Cooking")));
-		cookingXpLabel.setText(Integer.toString(user.getLevelObject("Cooking").getXp()));
 		miningLabel.setText(Integer.toString(user.getLevel("Mining")));
-		miningXpLabel.setText(Integer.toString(user.getLevelObject("Mining").getXp()));
 		smithingLabel.setText(Integer.toString(user.getLevel("Smithing")));
-		smithingXpLabel.setText(Integer.toString(user.getLevelObject("Smithing").getXp()));
 		herbloreLabel.setText(Integer.toString(user.getLevel("Herblore")));
-		herbloreXpLabel.setText(Integer.toString(user.getLevelObject("Herblore").getXp()));
 	}
-	
-    // Constructor to setup the GUI components
-    public GUIWindow(String userName, String userClass) {
-    	user = Play.startUser(userName, userClass, false);
-    	
-    	setLayout(new FlowLayout());
-	    	// "super" Frame, which is a Container, sets its layout to FlowLayout to arrange
-	        // the components from left-to-right, and flow to next row from top-to-bottom.
-    	Panel status = new Panel();
-    	Panel stats = new Panel();
-    	Panel exp = new Panel();
-    	Panel output = new Panel();
-    	Panel options = new Panel();
-    	
-    	
-    	// Main menu elements
-    	fishingLabel = new Label ("Fishing Lvl:" + user.getLevel("Fishing")+"      "); 
-    	stats.add(fishingLabel); 
-    	fishingXpLabel = new Label ("Fishing XP:" + user.getLevelObject("Fishing").getXp()+"      "); 
-    	exp.add(fishingXpLabel);
-    	cookingLabel = new Label ("Cooking Lvl:" + user.getLevel("Cooking")+"      "); 
-    	stats.add(cookingLabel); 
-    	cookingXpLabel = new Label ("Cooking XP:" + user.getLevelObject("Cooking").getXp()+"      "); 
-    	exp.add(cookingXpLabel);
-    	miningLabel = new Label ("Mining Lvl:" + user.getLevel("Mining")+"      "); 
-    	stats.add(miningLabel);  
-    	miningXpLabel = new Label ("Mining XP:" + user.getLevelObject("Mining").getXp()+"      "); 
-    	exp.add(miningXpLabel);
-    	smithingLabel = new Label ("Smithing Lvl:" + user.getLevel("Smithing")+"      "); 
-    	stats.add(smithingLabel);
-    	smithingXpLabel = new Label ("Smithing XP:" + user.getLevelObject("Smithing").getXp()+"      "); 
-    	exp.add(smithingXpLabel);
-    	herbloreLabel = new Label ("Herblore Lvl:" + user.getLevel("Herblore")+"      "); 
-    	stats.add(herbloreLabel);
-    	herbloreXpLabel = new Label ("Herblore XP:"+ user.getLevelObject("Herblore").getXp()+"      "); 
-    	exp.add(herbloreXpLabel);
 
-    	
-    	//Console window
-    	if(!user.userName.isEmpty()){ // if no name is entered, skip to else statement
-    		messages = new TextField("Welcome to miniRPG, "+user.userName+"!", 55); // construct the TextField component
-    	}
-    	else{
-    		messages = new TextField("Welcome to miniRPG! You didn't introduce yourself; I'll call you Bobbert.",55);
-    		user.userName = "Bobbert";
-    	}
-        messages.setEditable(false);       // set to read-only
-        output.add(messages);              // "super" Frame container adds TextField component
-        
-        //Construct buttons
-        combatSelectButton = new Button("Combat");
-        InventorySelectButton = new Button("Inventory");
-        EquippedSelectButton = new Button("Equipment"); 
-        ShopSelectButton = new Button("Shop"); 
-        HospitalSelectButton = new Button("Hospital");  
-        FishingSelectButton = new Button("Fish");  
-        MiningSelectButton = new Button("Mine");    
-        QuitSelectButton  = new Button("Quit");
-        combatStatsSelectButton = new Button(user.userClass+" Stats");
-        options.add(combatSelectButton);
-        options.add(combatStatsSelectButton);
-        options.add(InventorySelectButton);
-        options.add(EquippedSelectButton);
-        options.add(ShopSelectButton);
-        options.add(HospitalSelectButton);
-        options.add(FishingSelectButton);
-        options.add(MiningSelectButton);
-        options.add(QuitSelectButton);
-        combatSelectButton.addActionListener(this);
-        InventorySelectButton.addActionListener(this);
-        EquippedSelectButton.addActionListener(this);
-        ShopSelectButton.addActionListener(this);
-        HospitalSelectButton.addActionListener(this);
-        FishingSelectButton.addActionListener(this);
-        MiningSelectButton.addActionListener(this);
-        QuitSelectButton.addActionListener(this);
-        combatStatsSelectButton.addActionListener(this);
-        
-        addWindowListener(this);
-        // "super" Frame (source object) fires WindowEvent.
-        // "super" Frame adds "this" object as a WindowEvent listener.
-        
-        setTitle("miniRPG");  // "super" Frame sets its title
-        setSize(650, 200);    // "super" Frame sets its initial window size
-        
-        setVisible(true); //"super" Frame shows
-        
-        add(status);
-        add(stats);
-        add(exp);
-        add(output);
-        add(options);
-    }       
+	// Constructor to setup the GUI components
+	public GUIWindow(String userName, String userClass, boolean hardcoreMode) throws IOException {
+		user = Play.startUser(userName, userClass, hardcoreMode);
+		Play.saveData(user);
+		setLayout(new FlowLayout());
+		// "super" Frame, which is a Container, sets its layout to FlowLayout to arrange
+		// the components from left-to-right, and flow to next row from top-to-bottom.
+		Panel status = new Panel();
+		Panel stats = new Panel();
+		Panel stats2 = new Panel();
+		Panel exp = new Panel();
+		Panel output = new Panel();
+		Panel options = new Panel();
 
-    //ActionEvent handler 
-    @Override
-    public void actionPerformed(ActionEvent evt) {
-    	if(evt.getSource() == FishingSelectButton){
-    		status = Fishing.fishing(user);
-    		messages.setText(status);
-    		messages.validate();
-    		fishingLabel.setText("Fishing Lvl:" + user.getLevel("Fishing")); 
-        	fishingXpLabel.setText("Fishing XP:" + user.getLevelObject("Fishing").getXp()); 
-    	}
-    	if(evt.getSource() == MiningSelectButton) {
-    		status = Mining.mining(user);
-    		messages.setText(status);
-    		messages.validate();
-    		miningLabel.setText("Mining Lvl:" + user.getLevel("Mining"));
-    		miningXpLabel.setText("Mining XP:" + user.getLevelObject("Mining").getXp());
-    	}
-    	if(evt.getSource() == InventorySelectButton){
-    		inventoryWindow frame = new inventoryWindow(user);
-    		frame.setVisible(true);
-    	}
-    	if(evt.getSource() == combatStatsSelectButton){
-    		combatStatsWindow frame = new combatStatsWindow(user);
-    		frame.setVisible(true);
-    	}
-    	if(evt.getSource() == HospitalSelectButton){
-    		hospitalWindow frame = new hospitalWindow(user);
-    		frame.setVisible(true);
-    	}
-    	if(evt.getSource() == combatSelectButton){
-    		//TODO: Remove the following line and make real combat
-    		user.setCurrentHealth(user.getCurrentHealth()-5);
-    	}
-    	if(evt.getSource() == ShopSelectButton){
-    		//TODO: Remove the following line and make real shop
-    		user.setMoney(user.getMoney()+5);
-    	}
-    	if(evt.getSource() == QuitSelectButton){
-    		System.exit(0);
-    	}
-    }
+		// Main menu elements
+		Level level = user.getLevelObject("Fishing");
+		fishingLabel.setText("Fishing: (" + level.getLevelFromXP(level.getXp()) + ") " + level.getXp() + "/"
+				+ level.getXPFromLevel(level.getLevelFromXP(level.getXp()) + 1));
+		stats.add(fishingLabel);
+		level = user.getLevelObject("Cooking");
+		cookingLabel.setText("Cooking: (" + level.getLevelFromXP(level.getXp()) + ") " + level.getXp() + "/"
+				+ level.getXPFromLevel(level.getLevelFromXP(level.getXp()) + 1));
+		stats.add(cookingLabel);
+		level = user.getLevelObject("Mining");
+		miningLabel.setText("Mining: (" + level.getLevelFromXP(level.getXp()) + ") " + level.getXp() + "/"
+				+ level.getXPFromLevel(level.getLevelFromXP(level.getXp()) + 1));
+		stats2.add(miningLabel);
+		level = user.getLevelObject("Smithing");
+		smithingLabel.setText("Smithing: (" + level.getLevelFromXP(level.getXp()) + ") " + level.getXp() + "/"
+				+ level.getXPFromLevel(level.getLevelFromXP(level.getXp()) + 1));
+		stats2.add(smithingLabel);
+		level = user.getLevelObject("Herblore");
+		herbloreLabel.setText("Herblore: (" + level.getLevelFromXP(level.getXp()) + ") " + level.getXp() + "/"
+				+ level.getXPFromLevel(level.getLevelFromXP(level.getXp()) + 1));
+		stats.add(herbloreLabel);
+
+		// Console window
+		if (!user.userName.isEmpty()) { // if no name is entered, skip to else statement
+			messages = new TextField("Welcome to miniRPG, " + user.userName + "!", 55); // construct the TextField
+		} else {
+			messages = new TextField("Welcome to miniRPG! You didn't introduce yourself; I'll call you Bobbert.", 55);
+			user.userName = "Bobbert";
+		}
+		messages.setEditable(false); // set to read-only
+		output.add(messages); // "super" Frame container adds TextField component
+
+		// Construct buttons
+		combatSelectButton = new Button("Combat");
+		InventorySelectButton = new Button("Inventory");
+		EquippedSelectButton = new Button("Equipment");
+		ShopSelectButton = new Button("Shop");
+		HospitalSelectButton = new Button("Hospital");
+		FishingSelectButton = new Button("Fish");
+		MiningSelectButton = new Button("Mine");
+		QuitSelectButton = new Button("Quit");
+		combatStatsSelectButton = new Button(user.userClass + " Stats");
+		options.add(combatSelectButton);
+		options.add(combatStatsSelectButton);
+		options.add(InventorySelectButton);
+		options.add(EquippedSelectButton);
+		options.add(ShopSelectButton);
+		options.add(HospitalSelectButton);
+		options.add(FishingSelectButton);
+		options.add(MiningSelectButton);
+		options.add(QuitSelectButton);
+		combatSelectButton.addActionListener(this);
+		InventorySelectButton.addActionListener(this);
+		EquippedSelectButton.addActionListener(this);
+		ShopSelectButton.addActionListener(this);
+		HospitalSelectButton.addActionListener(this);
+		FishingSelectButton.addActionListener(this);
+		MiningSelectButton.addActionListener(this);
+		QuitSelectButton.addActionListener(this);
+		combatStatsSelectButton.addActionListener(this);
+
+		addWindowListener(this);
+		// "super" Frame (source object) fires WindowEvent.
+		// "super" Frame adds "this" object as a WindowEvent listener.
+
+		setTitle("miniRPG"); // "super" Frame sets its title
+		setSize(650, 200); // "super" Frame sets its initial window size
+
+		setVisible(true); // "super" Frame shows
+
+		add(status);
+		add(stats);
+		add(stats2); // TODO : PRINT ON NEW LINE
+		add(exp);
+		add(output);
+		add(options);
+	}
+
+	// ActionEvent handler
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		if (evt.getSource() == FishingSelectButton) {
+			status = Fishing.fishing(user);
+			messages.setText(status);
+			messages.validate();
+			Level level = user.getLevelObject("Fishing");
+			fishingLabel.setText("Fishing: (" + level.getLevelFromXP(level.getXp()) + ") " + level.getXp() + "/"
+					+ level.getXPFromLevel(level.getLevelFromXP(level.getXp()) + 1));
+		}
+		if (evt.getSource() == MiningSelectButton) {
+			status = Mining.mining(user);
+			messages.setText(status);
+			messages.validate();
+			miningLabel.setText("Mining Lvl:" + user.getLevel("Mining"));
+			Level level = user.getLevelObject("Mining");
+			miningLabel.setText("Mining: (" + level.getLevelFromXP(level.getXp()) + ") " + level.getXp() + "/"
+					+ level.getXPFromLevel(level.getLevelFromXP(level.getXp()) + 1));
+		}
+		if (evt.getSource() == InventorySelectButton) {
+			inventoryWindow frame = new inventoryWindow(user);
+			frame.setVisible(true);
+		}
+		if (evt.getSource() == combatStatsSelectButton) {
+			combatStatsWindow frame = new combatStatsWindow(user);
+			frame.setVisible(true);
+		}
+		if (evt.getSource() == HospitalSelectButton) {
+			hospitalWindow frame = new hospitalWindow(user);
+			frame.setVisible(true);
+		}
+		if (evt.getSource() == combatSelectButton) {
+			// TODO: Remove the following line and make real combat
+			user.setCurrentHealth(user.getCurrentHealth() - 5);
+		}
+		if (evt.getSource() == ShopSelectButton) {
+			// TODO: Remove the following line and make real shop
+			user.setMoney(user.getMoney() + 5);
+		}
+		if (evt.getSource() == QuitSelectButton) {
+			try {
+				Play.saveData(user); // TODO : SAVE AFTER ACTION
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.exit(0);
+		}
+	}
 
 	/* WindowEvent handlers */
-    // Called back upon clicking close-window button
-    @Override
-    public void windowClosing(WindowEvent evt) {
-        System.exit(0);  // Terminate the program
-    }
-  
-    // Not Used, but need to provide an empty body to compile.
-    @Override public void windowOpened(WindowEvent evt) { }
-    @Override public void windowClosed(WindowEvent evt) { }
-    @Override public void windowIconified(WindowEvent evt) { }
-    @Override public void windowDeiconified(WindowEvent evt) { }
-    @Override public void windowActivated(WindowEvent evt) { }
-    @Override public void windowDeactivated(WindowEvent evt) { }
+	// Called back upon clicking close-window button
+	@Override
+	public void windowClosing(WindowEvent evt) {
+		System.exit(0); // Terminate the program
+	}
+
+	// Not Used, but need to provide an empty body to compile.
+	@Override
+	public void windowOpened(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowClosed(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowIconified(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowActivated(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent evt) {
+	}
 
 }
--- src/miniRPG/startGameGui.java
+++ src/miniRPG/startGameGui.java
@@ -1,129 +1,212 @@
 package miniRPG;
 
-import java.awt.*;  // Using Frame class in package java.awt
-import java.awt.event.*;
-import javax.swing.JProgressBar;
+// Using Frame class in package java.awt
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.List;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.IOException;
 
-public class startGameGui extends Frame
-implements ActionListener, WindowListener {		
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+
+public class startGameGui extends Frame implements ActionListener, WindowListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	// UI Elements
 	private Label nameLabel;
 	private Label classLabel;
+	private Label hardcoreModeLabel;
 	private List classList;
 	private Button cancelButton;
 	private Button confirmButton;
 	private TextField nameEntry;
-	
+	JRadioButton noButton;
+	JRadioButton yesButton;
+
 	// Selection var for classList
 	private String userClass;
 	private String userName;
-	
-	public startGameGui(){
+	private boolean hardcoreMode;
+
+	public startGameGui() {
 		setLayout(new FlowLayout());
-		
+
 		Panel name = new Panel();
 		Panel classSelect = new Panel();
+		Panel hardcoreSelect = new Panel();
 		Panel buttons = new Panel();
-		
+
 		// Menu elements
-		nameLabel = new Label ("Enter your name:");
-		nameEntry = new TextField("",30);
+		nameLabel = new Label("Enter your name:");
+		nameEntry = new TextField("", 30);
 		classLabel = new Label("Select a class:");
 		classList = new List(7, false);
-			classList.add("Barbarian (+25 ATK; -15 HP)");
-			classList.add("Knight (+15 DEF; +20 HP");
-			classList.add("Thief (+10 ATK; +5% GP Gain");
-			classList.add("Warrior (+10 ATK; +10 DEF");
-			classList.add("Chicken Tender (Random Stats)");
-			classList.select(0);
-			userClass = "Barbarian";
+		hardcoreModeLabel = new Label("Hardcore mode? (reset on death)");
+
+		yesButton = new JRadioButton("Yes");
+		yesButton.setMnemonic(KeyEvent.VK_B);
+		yesButton.setActionCommand("Yes");
+		yesButton.setSelected(false);
+
+		noButton = new JRadioButton("No");
+		noButton.setMnemonic(KeyEvent.VK_B);
+		noButton.setActionCommand("No");
+		noButton.setSelected(true);
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(noButton);
+		group.add(yesButton);
+
+		classList.add("Barbarian (+20 ATK; -15 HP, -5 SPD)");
+		classList.add("Knight (-5 ATK, +15 DEF; +20 HP)");
+		classList.add("Thief (+10 ATK; +5% GP Gain, +5 SPD)");
+		classList.add("Warrior (+10 ATK; +10 DEF");
+		classList.add("Chicken Tender (Random Stats)");
+		classList.select(0);
+		userClass = "Barbarian";
 		cancelButton = new Button("Quit");
 		confirmButton = new Button("Start!");
-		
+
+		hardcoreMode = false;
+
 		// add listeners
 		cancelButton.addActionListener(this);
 		confirmButton.addActionListener(this);
 		classList.addActionListener(this);
-		
+		noButton.addActionListener(this);
+		yesButton.addActionListener(this);
+
+		noButton.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				if (noButton.isSelected()) {
+					hardcoreMode = false;
+				} else {
+					hardcoreMode = true;
+				}
+			}
+		});
+
 		// classList Listener definition
-		classList.addItemListener(new ItemListener(){
-			public void itemStateChanged(ItemEvent ievt){
-				if(classList.getSelectedIndex()==0){
+		classList.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent ievt) {
+				if (classList.getSelectedIndex() == 0) {
 					userClass = "Barbarian";
-				}
-				else if(classList.getSelectedIndex()==1){
+				} else if (classList.getSelectedIndex() == 1) {
 					userClass = "Knight";
-				}
-				else if(classList.getSelectedIndex()==2){
+				} else if (classList.getSelectedIndex() == 2) {
 					userClass = "Thief";
-				}
-				else if(classList.getSelectedIndex()==3){
+				} else if (classList.getSelectedIndex() == 3) {
 					userClass = "Warrior";
-				}
-				else if(classList.getSelectedIndex()==4){
+				} else if (classList.getSelectedIndex() == 4) {
 					userClass = "Chicken Tender";
-				}
-				else{
+				} else {
 					userClass = "Barbarian"; // error handling
 				}
 			}
 		});
-		
-		
+
 		// add elements
 		name.add(nameLabel);
 		name.add(nameEntry);
 		classSelect.add(classLabel);
 		classSelect.add(classList);
+		hardcoreSelect.add(hardcoreModeLabel);
+		hardcoreSelect.add(noButton);
+		hardcoreSelect.add(yesButton);
 		buttons.add(cancelButton);
 		buttons.add(confirmButton);
-		
+
 		/* Window Setup */
 		addWindowListener(this);
-        // "super" Frame (source object) fires WindowEvent.
-        // "super" Frame adds "this" object as a WindowEvent listener.
+		// "super" Frame (source object) fires WindowEvent.
+		// "super" Frame adds "this" object as a WindowEvent listener.
 		setTitle("Configure your character");
-		setSize(400,250);
+		setSize(400, 300);
 		setVisible(true);
-		
+
 		add(name);
 		add(classSelect);
+		add(hardcoreSelect);
 		add(buttons);
+
 	}
-	
+
 	// The entry main() method
-    public static void main(String[] args) {
-        // Invoke the constructor (to set up the GUI) by allocating an instance
-    	new startGameGui();
-    }
-	
-	
-	//ActionEvent handler 
-    @Override
-    public void actionPerformed(ActionEvent evt) {
-    	if(evt.getSource() == cancelButton){
-    		System.exit(0);
-    	}
-    	if(evt.getSource() == confirmButton){
-    		userName = nameEntry.getText();
-    		GUIWindow frame = new GUIWindow(userName, userClass);
-    		frame.setVisible(true);
-    		this.setVisible(false);
-    	}
-    }
-    /* WindowEvent handlers */
-    // Called back upon clicking close-window button
-    @Override
-    public void windowClosing(WindowEvent evt) {
-        System.exit(0);  // Terminate the program
-    }
-  
-    // Not Used, but need to provide an empty body to compile.
-    @Override public void windowOpened(WindowEvent evt) { }
-    @Override public void windowClosed(WindowEvent evt) { }
-    @Override public void windowIconified(WindowEvent evt) { }
-    @Override public void windowDeiconified(WindowEvent evt) { }
-    @Override public void windowActivated(WindowEvent evt) { }
-    @Override public void windowDeactivated(WindowEvent evt) { }
+	public static void main(String[] args) throws IOException {
+		// Invoke the constructor (to set up the GUI) by allocating an instance
+		User user = Play.loadData();
+		if (user == null) {
+			new startGameGui();
+		} else {
+			GUIWindow frame = new GUIWindow(user.userName, user.userClass, user.hardcoreMode);
+			frame.setVisible(true);
+		}
+	}
+
+	// ActionEvent handler
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		if (evt.getSource() == cancelButton) {
+			System.exit(0);
+		}
+		if (evt.getSource() == confirmButton) {
+			userName = nameEntry.getText();
+			GUIWindow frame;
+			try {
+				frame = new GUIWindow(userName, userClass, hardcoreMode);
+				frame.setVisible(true);
+				this.setVisible(false);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	/* WindowEvent handlers */
+	// Called back upon clicking close-window button
+	@Override
+	public void windowClosing(WindowEvent evt) {
+		System.exit(0); // Terminate the program
+	}
+
+	// Not Used, but need to provide an empty body to compile.
+	@Override
+	public void windowOpened(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowClosed(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowIconified(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowActivated(WindowEvent evt) {
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent evt) {
+	}
 
 }
